c2cc962c63b967c0af97b52a7750b2f015503fbf
Habib Benharrat <benharrat.habib1@gmail.com>
2019-11-05 15:15:47 +0100

    ajout clé primair de la classe Borrow

--- src/medianetapp/model/Borrow.php
+++ src/medianetapp/model/Borrow.php
@@ -8,7 +8,7 @@ class Borrow extends \Illuminate\Database\Eloquent\Model
 {
 
     protected $table      = 'Borrow';  /* le nom de la table */
-    protected $primaryKey = '';     /* le nom de la clé primaire */
+    protected $primaryKey = 'id';     /* le nom de la clé primaire */
     public    $timestamps = false;    /* si vrai la table doit contenir
                                       les deux colonnes updated_at,
                                       created_a*/
