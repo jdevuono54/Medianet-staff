0f532ad7bd2d3120aa5f2b0edc2a8a36070dffaa
jdevuono <jacques.devuono@gmail.com>
2019-11-05 14:49:10 +0100

    création controller

---
+++ src/medianetapp/control/MedianetController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace medianetapp\control;
+
+
+class MedianetController extends \mf\control\AbstractController
+{
+    public function __construct(){
+        parent::__construct();
+    }
+}
