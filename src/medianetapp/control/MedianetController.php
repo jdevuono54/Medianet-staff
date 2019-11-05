f29b6453a2f8e8021d9bcf3fbb1c5666894f210e
jdevuono <jacques.devuono@gmail.com>
2019-11-06 00:25:30 +0100

    Gestion des erreurs et envoie dans la bdd ok

--- src/medianetapp/control/MedianetController.php
+++ src/medianetapp/control/MedianetController.php
@@ -26,38 +26,73 @@ class MedianetController extends \mf\control\AbstractController
     }
 
     public function add_borrow(){
-
+        /* VERFIE QUE LES CHAMPS NE SONT PAS VIDES */
         if($_POST["user"] != null && $_POST["documents"] != null){
-            $user = User::where("id","=",$_POST["user"])->first();
+            $user = User::where("id","=",filter_var($_POST["user"],FILTER_SANITIZE_NUMBER_INT))->first();
 
+            /* VERIFIE QUE L'UTILISATEUR EXISTE SINON RENVOIE SUR LA PAGE AVEC MSG ERREUR */
             if($user === null){
                 $vue = new MedianetView(["error_message" => "L'utilisateur n'existe pas"]);
                 $vue->render("borrow");
             }
             else{
+                /* SI USER EXISTE TRANSFORME LE CHAMP DE REF DE DOC EN TABLEAU, PREPARE UNE COLLECTION DE DOCUMENTS ET UNE COLLECTION DE MESSAGE D'ERREUR POUR SAVOIR QUELLE REF BUG */
                 $references = explode(",",$_POST["documents"]);
                 $error_reference = [];
+                $documents = [];
 
-
+                /* PARCOURS LES REFERENCES ET SELECT CHAQUE REF DANS LA BASE, SI EXISTE AJOUTE LE DOC DANS LE TABLEAU DOC SINON AJOUTE LA REF AU TABLEAU D'ERREUR */
                 foreach ($references as $reference){
+                    $reference = filter_var($reference,FILTER_SANITIZE_NUMBER_INT);
                     $document = Document::where("reference", "=", $reference)->first();
 
                     if($document === null){
                         $error_reference[] = $reference;
                     }
+                    else{
+                        $documents[] = $document;
+                    }
                 }
 
+                /* SI TABLEAU ERREUR EST VIDE VERIFIE QUE L'ETAT EST DISPONIBLE SINON AJOUTE LA REF DANS TABLEAU ERREUR */
                 if($error_reference === []){
-                    echo "ok";
+                    foreach ($documents as $document){
+                        if($document->id_State != 1){
+                            $error_reference[] = $document->reference;
+                        }
+                    }
+                        /* SI TABLEAU ERREUR EST VIDE ENREGISTRE L'EMPRUNT DANS EN BDD ET MODIFIE ETAT DOCUMENT */
+                        if($error_reference === []){
+                            foreach ($documents as $document){
+                                $borrow = new Borrow;
+
+                                $date = date("Y-m-d");
+                                $return_date = date("Y-m-d",strtotime($date."+7 day"));
+
+                                $borrow->id_User = filter_var($_POST["user"],FILTER_SANITIZE_NUMBER_INT);
+                                $borrow->id_Document = $document->id;
+                                $borrow->borrow_date = $date;
+                                $borrow->return_date = $return_date;
+
+                                $borrow->save();
+
+                                $document->id_State=2;
+                                $document->update();
+                            }
+                        }else{
+                            /* SI DOCUMENT INDISPONIBLE */
+                            $message_erreur = $this->errorMessage("Ajout d'emprunt échoué, la ou les référence(s) :",$error_reference);
+                            $message_erreur .= " est/sont indisponible.";
+
+                            $vue = new MedianetView(["error_message" => $message_erreur]);
+                            $vue->render("borrow");
+                        }
+
+
                 }
                 else{
-                    $message_erreur = "Ajout d'emprunt échoué, la ou les référence(s) : ";
-
-                    foreach ($error_reference as $reference){
-                        $message_erreur .= $reference.",";
-                    }
-
-                   $message_erreur = substr($message_erreur, 0, -1);
+                    /* SI DOCUMENT EXISTE PAS */
+                    $message_erreur = $this->errorMessage("Ajout d'emprunt échoué, la ou les référence(s)",$error_reference);
                     $message_erreur .= " n'éxiste(s) pas.";
 
                     $vue = new MedianetView(["error_message" => $message_erreur]);
@@ -66,12 +101,23 @@ class MedianetController extends \mf\control\AbstractController
             }
         }
         else{
+            /* SI FORMULAIRE NON REMPLI */
             $vue = new MedianetView(["error_message" => "Veuillez saisir l'id de l'utilisateur et l'id du/des document(s)"]);
             $vue->render("borrow");
         }
+    }
 
+    /* PERMET DE CREE UN MESSAGE D'ERREUR ET D'AFFICHER LES SOURCES DE L'ERREUR */
+    private function errorMessage($startMessage, $error_target = []){
+        $message_erreur = " ".$startMessage." ";
 
+        if($error_target != []){
+            foreach ($error_target as $target){
+                $message_erreur .= $target.",";
+            }
+            $message_erreur = substr($message_erreur, 0, -1);
+        }
 
-
+        return $message_erreur;
     }
 }
