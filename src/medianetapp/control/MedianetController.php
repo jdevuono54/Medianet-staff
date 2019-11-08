cb9088049c7f273e92f9605b1afa7a593c62925d
oualid bammou <bammou.ceo@gmail.com>
2019-11-08 16:08:48 +0100

    créér / valider une inscription par le staff

--- src/medianetapp/control/MedianetController.php
+++ src/medianetapp/control/MedianetController.php
@@ -7,12 +7,19 @@ namespace medianetapp\control;
 use medianetapp\model\Borrow;
 use medianetapp\model\Document;
 use medianetapp\model\User;
+use medianetapp\model\SignUpRequest;
 use medianetapp\view\MedianetView;
+use mf\router\Router;
 
 class MedianetController extends \mf\control\AbstractController
 {
     public function __construct(){
         parent::__construct();
+    }
+
+    public function viewHome(){
+        $vue = new MedianetView(null);
+        $vue->render("home");
     }
     /*
      * Display the return view
@@ -86,4 +93,74 @@ class MedianetController extends \mf\control\AbstractController
             }
             return $unreturnedDocuments  ;
         }
+
+        /*
+         * Display the Check Signup Request
+         */
+        public function viewCheckSignupRequest(){
+            $view = new MedianetView(null);
+            $view->render("check_signup_request");
+        }
+
+        /*
+         * Validate a Signup Request
+         */
+        public function validateSignupRequest(){
+            if(isset($_REQUEST['txtMail'])) {
+                $mail = $_REQUEST['txtMail'];
+                $demande = SignUpRequest::where("mail", "=", $mail)->first();
+                $newUser = new User();
+                $newUser->name = $demande->name;
+                $newUser->mail = $demande->mail;
+                $newUser->password = $demande->password;
+                $newUser->phone = $demande->phone;
+                $newUser->membership_date = date("Y-m-d");
+                $newUser->save();
+
+                $demande->delete();
+                $view = new MedianetView(null);
+                $view->render("check_signup_request");
+            }
+        }
+
+
+        public function addUser(){
+            if(isset($_REQUEST['txtName']) && isset($_REQUEST['txtMail']) && isset($_REQUEST['txtPassword1'])
+            && isset($_REQUEST['txtPassword2']) && isset($_REQUEST['txtPhone'])) {
+
+                /*Valeurs filtrés*/
+                $name = strip_tags(trim($_REQUEST['txtName']));
+                $mail = strip_tags(trim($_REQUEST['txtMail']));
+                $password1 = $_REQUEST['txtPassword1'];
+                $password2 = $_REQUEST['txtPassword2'];
+                $phone = strip_tags(trim($_REQUEST['txtPhone']));
+
+                if(User::where('mail','=',$mail)->first() or SignupRequest::where('mail','=',$mail)->first()) {
+                    $view = new MedianetView(["error_message"=>"L'adresse email existe déja!"]);
+                    $view->render("check_signup_request");
+                }
+                else {
+                    if($password1 != $password2) {
+                        $view = new MedianetView(["error_message"=>"Les champs de mot de passe ne sont pas identiques!"]);
+                        $view->render("check_signup_request");
+                    }
+                    else {
+                        $newUser = new User();
+                        $newUser->name = $name;
+                        $newUser->mail = $mail;
+                        $newUser->password = $this->hashPassword($password1);
+                        $newUser->phone = $phone;
+                        $newUser->membership_date = date("Y-m-d");
+                        $newUser->save();
+                        Router::executeRoute("home");
+                    }
+                }
+            }
+    }
+
+
+    protected function hashPassword($password){
+        return password_hash($password, PASSWORD_DEFAULT);
+    }
+
 }
--- src/medianetapp/view/MedianetView.php
+++ src/medianetapp/view/MedianetView.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace medianetapp\view;
+use medianetapp\model\SignUpRequest;
 
 class MedianetView extends \mf\view\AbstractView
 {
@@ -14,14 +15,23 @@ class MedianetView extends \mf\view\AbstractView
     private function renderFooter(){
 
     }
+    private function renderHome(){
+        return "home";
+    }
 
     protected function renderBody($selector=null){
         switch ($selector){
+            case "home":
+                $body = $this->renderHome();
+                break;
             case "return":
                 $body = $this->renderFormReturn();
                 break;
             case "return_recap":
                 $body=$this->renderRecap();
+                break;
+            case "check_signup_request":
+                $body=$this->renderCheckSignupRequest();
                 break;
 
         }
@@ -59,4 +69,71 @@ class MedianetView extends \mf\view\AbstractView
 
         return $contents;
     }
+
+    /////
+    private function renderCheckSignupRequest(){
+        $demandes = SignUpRequest::all();
+        $errorMessage="";
+        $message="";
+        if(isset($this->data["error_message"])){
+            $errorMessage="<p class='error_message'>{$this->data["error_message"]}</p>";
+        }
+        $content="<h1 class='nomPage'>Ajouter un usager</h1>
+                  <div id='signup_form'>
+                    <form method='post' action ='add_user'>
+                    <div>
+                        <label for='txtName'>Nom :</label>
+                        <input type = 'text' name = 'txtName' required/>
+                    </div>
+                    <div>
+                        <label for='txtMail'>Mail :</label>
+                        <input type = 'email' name = 'txtMail' required/>
+                    </div>
+                    <div>
+                        <label for='txtPassword1'>Mot de passe :</label>
+                        <input type = 'password' name = 'txtPassword1' required/>
+                    </div>
+                    <div>
+                        <label for='txtPassword2'>Répeter le mot de passe :</label>
+                        <input type = 'password' name = 'txtPassword2' required/>
+                    </div>
+                    <div>
+                        <label for='txtPhone'>Tel (Format: 0123456789) :</label>
+                        <input type = 'tel' name = 'txtPhone' pattern='[0-9]{10}' required/>
+                    </div>
+                    <input type='submit' value='Enregistrer' value = 'Enregistrer'/>$errorMessage
+                    </form>
+                  </div>
+                  <h1 class='nomPage'>Les demandes d'inscription</h1>";
+
+        if(!$demandes){
+            $message="<p class='error_message'>Pas de demandes d'inscriptions</p>";
+        }
+        else {
+            foreach ($demandes as $demande) {
+                $content.="<h2 class='numDemande'>Demande $demande->id</h2>
+                         <div id='demande_form'>
+                            <form method='post' action ='validate_signup_request'>
+                            <div>
+                                <label for='txtName'>Nom :</label>
+                                <input type = 'text' value='$demande->name' name = 'txtName' disabled/>
+                            </div>
+                            <div>
+                                <label for='txtMail'>Mail :</label>
+                                <input type = 'email' value='$demande->mail'  name = 'txtMail' readonly/>
+                            </div>
+                            <div>
+                                <label for='txtPhone'>Tel :</label>
+                                <input type = 'tel' value='$demande->phone'  name = 'txtPhone' disabled/>
+                            </div>
+                            <input type='submit' value='Valider' value = 'Valider'/>
+                            </form>
+                        </div>";
+            }
+        }
+
+        return $content . $message;
+    }
+
+
 }
